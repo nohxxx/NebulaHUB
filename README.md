2a98c0e31e5fc1fddc677314e1d225fcd848bd45
Nohxx <57000532+nohxxx@users.noreply.github.com>
2024-08-01 13:58:59 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+## NebulaHUB
+NebulaHUB é um hub da Nebula Network com connect simples entre seus servidores.
+- [Discord](https://discord.gg/XEkzEaaTj8)
+- [Grupo da Steam](https://steamcommunity.com/groups/oficialnebulanetwork)
---
+++ index.js
@@ -0,0 +1,37 @@
+        function connectToServer() {
+            window.location.href = "steam://connect/131.196.196.196:27330"; /* Ip do servidor */
+            setTimeout(function() {
+                window.close();
+            }, 3500);
+        }
+		function connectToServer1() {
+            window.location.href = ""; /* Ip do servidor 2 */
+            setTimeout(function() {
+                window.close();
+            }, 3500);
+        }
+		function connectToServer2() {
+            window.location.href = ""; /* Ip do servidor 3*/
+            setTimeout(function() {
+                window.close();
+            }, 3500);
+        }
+		function connectToServer3() {
+            window.location.href = ""; /* Ip do servidor 4*/
+            setTimeout(function() {
+                window.close();
+            }, 3500);
+        }
+		function troll() {
+            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; /* Never gonna give you up */
+            setTimeout(function() {
+                window.close();
+            }, 1000);
+        }
+		function signatureCheck() {
+            var sig = "Nebula";
+            if (sig !== "Nebula") {
+                document.body.innerHTML = "<h1>Erro de Assinatura</h1><p>Por favor, entre em contato com o suporte.</p>";
+            }
+        }
+        signatureCheck();
