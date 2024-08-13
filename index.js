4cb7e7df97393af5c8fd29e3700fde45b4a5d962
Nohxx <57000532+nohxxx@users.noreply.github.com>
2024-08-12 20:30:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+        document.addEventListener('copy', function(e) {
+            e.preventDefault();
+        });
+
+        document.addEventListener('cut', function(e) {
+            e.preventDefault();
+        });
+
+        document.addEventListener('paste', function(e) {
+            e.preventDefault();
+        });
+        (function() {
+            var devtools = {
+                open: false,
+                orientation: null
+            };
+        
+            // Função para verificar se as ferramentas de desenvolvedor estão abertas
+            var threshold = 160; // Limite para detectar o painel devtools
+            setInterval(function() {
+                // Verifica se a janela foi redimensionada ou se há um elemento suspeito de devtools
+                var widthThreshold = window.outerWidth - window.innerWidth > threshold;
+                var heightThreshold = window.outerHeight - window.innerHeight > threshold;
+                var orientation = widthThreshold ? 'vertical' : 'horizontal';
+        
+                if (!(heightThreshold && widthThreshold) && ((window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)) {
+                    if (!devtools.open || devtools.orientation !== orientation) {
+                        devtools.open = true;
+                        devtools.orientation = orientation;
+        
+                        // Redirecionar para outra página quando as ferramentas de desenvolvedor são detectadas
+                        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+                    }
+                } else {
+                    devtools.open = false;
+                    devtools.orientation = null;
+                }
+            }, 500);
+        })();        
         function connectToServer() {
             window.location.href = "steam://connect/131.196.196.196:27330"; /* Ip do servidor */
             setTimeout(function() {
