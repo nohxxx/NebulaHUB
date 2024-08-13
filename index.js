53464bbf5c32036034aa5e2ff3438153bcf8716c
Nohxx <57000532+nohxxx@users.noreply.github.com>
2024-08-12 20:40:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-        document.addEventListener('copy', function(e) {
-            e.preventDefault();
-        });
-
-        document.addEventListener('cut', function(e) {
-            e.preventDefault();
-        });
-
-        document.addEventListener('paste', function(e) {
-            e.preventDefault();
-        });
-        (function() {
+        (function() { codigoquebrado/
             var devtools = {
                 open: false,
                 orientation: null
